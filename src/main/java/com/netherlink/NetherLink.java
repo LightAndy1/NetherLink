3584f7dd24eeaec524602bedac0c83e697b6fff7
Light Andy <andy.197197@yahoo.com>
2024-03-19 23:14:57 +0200

    chore: initial commit v0.0.1

---
+++ src/main/java/com/netherlink/NetherLink.java
@@ -0,0 +1,19 @@
+package com.netherlink;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NetherLink implements ModInitializer {
+
+  // This logger is used to write text to the console and the log file.
+  // It is considered best practice to use your mod id as the logger's name.
+  // That way, it's clear which mod wrote info, warnings, and errors.
+  public static final Logger LOGGER = LoggerFactory.getLogger("netherlink");
+
+  @Override
+  public void onInitialize() {
+
+    LOGGER.info("Hello Fabric world!");
+  }
+}
